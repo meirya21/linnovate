ab160e9e61b2d2f36448e8e7f1c996929d556c26
meirya21 <yaa.meir@gmail.com>
2023-05-15 13:25:10 +0300

    conf

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php
-echo Hello();
+<html>
+    <head>
+        <title>PHP Hello World!</title>
+    </head>
+    <body>
+        <?php echo '<h1>Hello World</h1>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
