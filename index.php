73473e2c79c00ae30b5e230b6f47d2a308571a6d
meirya21 <yaa.meir@gmail.com>
2023-05-17 11:06:17 +0300

    mime

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<html>
-    <head>
-        <title>PHP Hello World!</title>
-    </head>
-    <body>
-        <?php echo '<h1>Hello World</h1>'; ?>
-        <?php phpinfo(); ?>
-    </body>
-</html>
+<?php
+    echo "Hello World";
+?>
