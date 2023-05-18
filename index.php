6d37f627b36ec018b3f440f4e653afeb714d59ee
meirya21 <yaa.meir@gmail.com>
2023-05-18 13:04:31 +0300

    new

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-    echo "Hello World";
-?>
+<html>
+    <head>
+        <title>PHP Hello World!</title>
+    </head>
+    <body>
+        <?php echo '<h1>Hello World</h1>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
